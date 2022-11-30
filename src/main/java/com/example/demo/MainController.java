480b41c8c6a1892f5c1ddb5ef5d8c9ad94602fad
tree4553 <tree4553@naver.com>
2022-11-30 18:52:42 +0900

    main page

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,14 +2,15 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("data", "Hellooooo!!");
-        return "/hello.html";
+        return "hello";
     }
 }
